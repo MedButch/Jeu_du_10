1be1c78b6aed297bafa6af2b7bde6fd63c74a8e2
MedButch <C3r3b0s666>
2023-03-08 22:01:49 -0500

    Get playable cards

--- Player.py
+++ Player.py
@@ -2,14 +2,36 @@ import Cards
 
 class Player:
     def __init__(self, hand):
+        self.hand = []
         hand.sort(key=lambda x: x.number)
-        self.hand = hand
+        spades = list(filter(lambda x: x.number // 10 == 0, hand))
+        diamonds = list(filter(lambda x: x.number // 10 == 1, hand))
+        hearts = list(filter(lambda x: x.number // 10 == 2, hand))
+        clovers = list(filter(lambda x: x.number // 10 == 3, hand))
+        self.hand.append(spades)
+        self.hand.append(diamonds)
+        self.hand.append(hearts)
+        self.hand.append(clovers)
+
+    #Get playable cards in hand
+    #Return suit of round or all other suits if that suit is not held
+    def playableCards(self, suit):
+        result = []
+        #empty list is False
+        if self.hand[suit]:
+            result = self.hand[suit]
+        else:
+            for i in range(4):
+                if i != suit and self.hand[i]:
+                    result.append(self.hand[i])
+        return result
+
 
     #Print out cards in hand
-    #Todo: show playable cards
     def printHand(self):
         output = ""
-        for card in self.hand:
-            output = output + str(card)
-            output = output + " "
+        for suit in self.hand:
+            for card in suit:
+                output = output + str(card)
+                output = output + " "
         print(output)
